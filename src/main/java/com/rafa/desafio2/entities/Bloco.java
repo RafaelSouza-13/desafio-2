c8ce404c4f79769378467aa3b0d06080d2190b53
Rafael Souza <rafa.nds13@gmail.com>
2024-04-14 14:52:18 -0300

    Implementando a classe bloco

---
+++ src/main/java/com/rafa/desafio2/entities/Bloco.java
@@ -0,0 +1,17 @@
+package com.rafa.desafio2.entities;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_bloco")
+public class Bloco {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant inicio;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant fim;
+}
